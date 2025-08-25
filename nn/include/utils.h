b827c5e82789530c88cfa62e39b992938c877785
Shreyash <shreyash1264@gmail.com>
2025-08-25 23:16:18 +0530

    style: update formatting

--- nn/include/utils.h
+++ nn/include/utils.h
@@ -1,40 +1,41 @@
 #pragma once
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <time.h>
 
 // Set up log levels
 typedef enum {
-    LOG_LEVEL_DEBUG,
-    LOG_LEVEL_INFO,
-    LOG_LEVEL_WARN,
-    LOG_LEVEL_ERROR
+  LOG_LEVEL_DEBUG,
+  LOG_LEVEL_INFO,
+  LOG_LEVEL_WARN,
+  LOG_LEVEL_ERROR
 } LogLevel;
 
 // Set the minimum log level to display. Change this to filter output.
 #define MIN_LOG_LEVEL LOG_LEVEL_INFO
 
 // Assert macro, it'll be useful for checking matrix properties
-#define ASSERT(condition, message) \
-    do { \
-        if (!(condition)) { \
-            fprintf(stderr, "Assertion failed in %s at line %d: %s\n", \
-                    __FILE__, __LINE__, message); \
-            exit(EXIT_FAILURE); \
-        } \
-    } while (0)
+#define ASSERT(condition, message)                                         \
+  do {                                                                     \
+    if (!(condition)) {                                                    \
+      fprintf(stderr, "Assertion failed in %s at line %d: %s\n", __FILE__, \
+              __LINE__, message);                                          \
+      exit(EXIT_FAILURE);                                                  \
+    }                                                                      \
+  } while (0)
 
 // For safe malloc allocations
-#define CHECK_MALLOC(ptr, message) \
-    ASSERT((ptr) != NULL, message);\
+#define CHECK_MALLOC(ptr, message) ASSERT((ptr) != NULL, message);
 
 // Function prototypes for logging
 void log_message(LogLevel level, const char* format, ...);
 
 // Convenience macros for easier logging
-#define LOG_DEBUG(format, ...) log_message(LOG_LEVEL_DEBUG, format, ##__VA_ARGS__)
-#define LOG_INFO(format, ...)  log_message(LOG_LEVEL_INFO, format, ##__VA_ARGS__)
-#define LOG_WARN(format, ...)  log_message(LOG_LEVEL_WARN, format, ##__VA_ARGS__)
-#define LOG_ERROR(format, ...) log_message(LOG_LEVEL_ERROR, format, ##__VA_ARGS__)
+#define LOG_DEBUG(format, ...) \
+  log_message(LOG_LEVEL_DEBUG, format, ##__VA_ARGS__)
+#define LOG_INFO(format, ...) log_message(LOG_LEVEL_INFO, format, ##__VA_ARGS__)
+#define LOG_WARN(format, ...) log_message(LOG_LEVEL_WARN, format, ##__VA_ARGS__)
+#define LOG_ERROR(format, ...) \
+  log_message(LOG_LEVEL_ERROR, format, ##__VA_ARGS__)
