13f9233def68c25d515eff8fd5f2c787ae08ebf7
Shreyash <shreyash1264@gmail.com>
2025-08-27 01:27:30 +0530

    fix: fix linting errors

--- nn/include/linalg.h
+++ nn/include/linalg.h
@@ -25,7 +25,7 @@ Matrix* flatten_matrix(Matrix* m, int axis);
 void fill_matrix(Matrix* m, double n);
 void randomize_matrix(Matrix* m, double n);
 void free_matrix(Matrix* m);
-void write_matrix(Matrix* m,const char* filename);
+void write_matrix(Matrix* m, const char* filename);
 void print_matrix(Matrix* m);
 int matrix_argmax(Matrix* m);
 
@@ -36,11 +36,8 @@ Matrix* identity_matrix(int n);
 Matrix* add_matrix(Matrix* a, Matrix* b);
 Matrix* subtract_matrix(Matrix* a, Matrix* b);
 Matrix* multiply_matrix(Matrix* a, Matrix* b);
-Matrix* apply_onto_matrix(double* (*func)(double),
-                          Matrix* m);  // Apply our own stuff onto the matrix
-                                       // items, useful for activation
+Matrix* apply_onto_matrix(double* (*func)(double), Matrix* m);
 Matrix* add_scalar_to_matrix(Matrix* m, double n);
 Matrix* dot_matrix(Matrix* a, Matrix* b);
 Matrix* transpose_matrix(Matrix* m);
-Matrix* scale_matrix(double n,
-                     Matrix* m);  // matrix can be scaled to non integer values
+Matrix* scale_matrix(double n, Matrix* m);
