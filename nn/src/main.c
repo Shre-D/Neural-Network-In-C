574daaf7bd10084d622b0e75b41fa2cb90da2b88
Shreyash <shreyash1264@gmail.com>
2025-09-19 03:24:18 +0530

    chore: update main, add doxygen comments

--- nn/src/main.c
+++ nn/src/main.c
@@ -1,5 +1,14 @@
+/**
+ * @file main.c
+ * @brief Main entry point for the neural network program.
+ */
+
 #include <stdio.h>
 
+/**
+ * @brief Main function of the program.
+ * @return 0 on successful execution.
+ */
 int main() {
   printf("NN Program");
   return 0;
